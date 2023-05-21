0914baaf480639d215c4652f880c812c811b2f3e
Mateo Estrada <mateoej12@gmail.com>
2023-05-21 13:34:45 -0700

    Adds server.

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+
+const { Configuration, OpenAIApi } = require("openai");
+
+// setup .env variables using dotenv
+require('dotenv').config();
+
+
+// middleware to parse the body
+app.use(express.json());
+
+// configure the openai api
+const configuration = new Configuration({
+  apiKey: process.env.apiKey,
+});
+const openai = new OpenAIApi(configuration);
+
+// make an async function to call the openai api and return the response
+// using the async await syntax and passing the prompt and model
+
+app.post('/calling-grammar-editor', async (req, res) => {
+  // Making a call to the openai api you need to pass the model and the messages
+  //  calling-grammar-editor
+  // { model: 'gpt-3.5-turbo', messages:
+  //  [{'role': 'system', 'content': 'you are a grammar editor bot'},
+  //   {'role': 'bot', 'content': 'I am a grammar editor bot'},
+  //  {'role', 'user', 'content': 'Can you fix my essays grammar?'}]}
+  const { model, messages } = req.body;
+  // parse the body and get the prompt and model
+  // make a try catch block to catch any errors
+  console.log(req.body);
+  try {
+    const gptResponse = await openai.createChatCompletion({ model: model, messages: messages, temperature: 0.5 });
+    console.log(gptResponse.data.choices[0].message, "the response");
+    res.status(200).json({ response: gptResponse.data.choices[0].message['content'] });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch completion from OpenAI' });
+  }
+});
+
+//get route
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+
+// Start the server and give the host and port
+// give the path to the server
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`)
+});
